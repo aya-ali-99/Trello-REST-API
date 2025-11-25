62cba2bf69eb846fc83354c9420fe458e26f3da8
aya-ali-99 <ayaali969@gmail.com>
2025-11-25 16:19:02 +0200

    Add hooks for board creation and deletion before and after scenarios

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -7,19 +7,24 @@ import java.io.IOException;
 
 public class Hooks {
 
+    @Before("@UpdateBoard or @DeleteBoard")
+    public void requiredBoardCreated() throws IOException {
+
+        StepDefinition s = new StepDefinition();
+        if(s.boardID == null) {
+            s.create_board_payload_with("RestTestBoard");
+            s.user_calls_with_http_request("CreateBoardAPI", "Post");
+            s.verify_board_exists_that_maps_to_using("RestTestBoard", "GetBoardAPI");
+        }
+    }
 
 
-//    @Before("@DeletePlace")
-//    public void beforeScenario() throws IOException {
-//
-//        StepDefinition s = new StepDefinition();
-//
-//        if(StepDefinition.placeID == null) {
-//            s.add_place_payload_with("Aya", "English", "Europe");
-//            s.user_calls_with_http_request("AddPlaceAPI", "Post");
-//            s.verify_place_id_created_that_maps_to_using("Aya", "GetPlaceAPI");
-//        }
-//    }
+
+    @After("@UpdateBoard or @DeleteBoard")
+    public void requiredBoardDeleted() throws IOException {
+        StepDefinition s = new StepDefinition();
+        s.user_calls_with_http_request("DeleteBoardAPI", "Delete");
+    }
 
 
 }
