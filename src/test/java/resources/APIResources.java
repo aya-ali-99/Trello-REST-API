f7f55f2724f1373a523fa90e9246b78a6d2211fd
aya-ali-99 <ayaali969@gmail.com>
2025-11-25 22:38:28 +0200

    Add Label API resources and organize enum with Board resources

--- src/test/java/resources/APIResources.java
+++ src/test/java/resources/APIResources.java
@@ -1,10 +1,16 @@
 package resources;
 
 public enum APIResources {
+    // Board
     CreateBoardAPI("/1/boards/"),
     GetBoardAPI("1/boards/{id}"),
     UpdateBoardAPI("1/boards/{id}"),
-    DeleteBoardAPI("1/boards/{id}");
+    DeleteBoardAPI("1/boards/{id}"),
+    // Label
+    CreateLabelAPI("/1/labels"),
+    GetLabelAPI("/1/labels/{id}"),
+    UpdateLabelAPI("/1/labels/{id}"),
+    DeleteLabelAPI("/1/labels/{id}");
 
     private String resource;
 
